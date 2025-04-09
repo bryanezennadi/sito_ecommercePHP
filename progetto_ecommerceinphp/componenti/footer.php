ccc4f3e3097f5786395b5da5a20ff4e3a450d220
bryan ezennadi <121224272+bryanezennadi@users.noreply.github.com>
2025-04-09 11:36:24 +0200

    Delete progetto_ecommerceinphp directory

--- progetto_ecommerceinphp/componenti/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="bg-black text-white text-center py-3">
-
-    <p>BRYAN EZENNADI 5F</p>
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2020 Copyright:
-        <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-    </div>
-</footer>
